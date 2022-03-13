24fc8997615dbee4643dc9fee50b10ace76c7a0e
radzhello <papa.rage@live.ru>
2022-03-13 23:41:20 +0300

    Task 2 - def rate_hw (correction)

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Reviewer(Mentor):
        self.surname = surname
        self.courses_attached = []
        
-    def rate_hw(self, student, course, grade):
+       def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
